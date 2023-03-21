5ca045bb9e49598b722f4e0bc75086aeea49f63a
Chachacharlie <TCCA_EDNS@hotmai.com>
2023-03-21 13:45:00 -0600

    animation cards job

--- src/components/Trabajo/Trabajo.jsx
+++ src/components/Trabajo/Trabajo.jsx
@@ -3,34 +3,56 @@ import React from "react";
 import '../../styles/trabajo.css'
 
 import { CardJob } from "./CardJob";
+import { useEffect, useRef  } from "react";
 
 export const Trabajo = ( ) =>{
+    const myRef  = useRef();
+
+    useEffect(() =>{
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting){
+                    console.log(entry)
+                    entry.target.classList.add('show');
+                }else{
+                    console.log(entry)
+                    
+                    entry.target.classList.remove('show');
+
+                }
+            })
+        })
+        const hiddenElemtens = document.querySelectorAll('.hidden')
+        hiddenElemtens.forEach((el) => observer.observe(el))
+    },[])
+
+
 
     return(
         <section id='Trabajo' className="section-basic-bg large">
-            <div className="container text-center mx-auto mt-4 overflow-hidden">
+            <div className="container text-center mx-auto mt-4 overflow-hidden ">
                 <div className="row g-2">
-                    <div className="col-md-4 ">
+                    <div className="col-md-4 hidden">
                         <CardJob icon={'bi bi-bus-front-fill'} name='Nearest neighbor algorithm' tags='Python | Algoritmos' url = 'https://github.com/Chachacharli/Nearest-neighbor-algoritm' ></CardJob>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-4 hidden">
                         <CardJob icon={'bi bi-box-seam-fill'} name='Knapsack problem greedy' tags='Python | Algoritmos | Notebook' url='https://github.com/Chachacharli/Knaspack-problem-Greedy-'></CardJob>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-4 hidden">
                         <CardJob icon={'bi bi-phone'} name='NutriApp' tags='React-Native | Android' url='https://github.com/Chachacharli/NutriApp'></CardJob>
                     </div>
 
                 </div>
                 <div className="row g-2">
-                <div className="col-md-8">
+                <div className="col-md-8 hidden">
                         <CardJob icon={'bi bi-award-fill'} name='NutriApp' tags='React-Native' url='https://github.com/Chachacharli/Knaspack-problem-Greedy-'></CardJob>
                     </div>
-                    <div className="col-md-4 ">
+                    <div className="col-md-4 hidden">
                         <CardJob icon={'bi bi-award-fill'} name='NutriApp' tags='React-Native' url='https://github.com/Chachacharli/Knaspack-problem-Greedy-'></CardJob>
                     </div>
                 </div>
                 <div className="row g-2">
-                <div className="col-md-12">
+                <div className="col-md-12 hidden">
                         <CardJob icon={'bi bi-globe'} name='CIIDIT Web' tags='NextJS | Jam-stack' url='https://rlucioporto.com/'></CardJob>
                     </div>
 
